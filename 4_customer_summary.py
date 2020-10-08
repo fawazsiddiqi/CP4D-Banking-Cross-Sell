73310f9563c1fc25822e54a5f914a855567ad166
mridulrb <mridurb@gmail.com>
2020-10-08 16:07:33 +0400

    Update 4_customer_summary.py

--- 4_customer_summary.py
+++ 4_customer_summary.py
@@ -7,7 +7,7 @@ def Merge_Customer_summary():
     df1 = pd.read_csv("Dataset-Final/customer_clean.csv")
     df2 = pd.read_csv("Dataset-Final/customer_summary_clean.csv")
 
-    df = df1.merge(df2, how='inner', on='CUSTOMER_ID')
+    df = df1.merge(df2, how='left', on='CUSTOMER_ID')
     # df = pd.concat([df1.reset_index(), df2], axis=1).set_index("CUSTOMER_ID")
     df.to_csv("Dataset-Final/merge_customer_summary.csv", index=False)
 
