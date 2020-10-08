cda65effe2aeac6907648faa6e1e392f4c6112d1
mridulrb <mridurb@gmail.com>
2020-10-08 16:01:46 +0400

    Create 4_customer_summary.py

---
+++ 4_customer_summary.py
@@ -0,0 +1,14 @@
+import re
+import csv
+import time
+import pandas as pd
+
+def Merge_Customer_summary():
+    df1 = pd.read_csv("Dataset-Final/customer_clean.csv")
+    df2 = pd.read_csv("Dataset-Final/customer_summary_clean.csv")
+
+    df = df1.merge(df2, how='inner', on='CUSTOMER_ID')
+    # df = pd.concat([df1.reset_index(), df2], axis=1).set_index("CUSTOMER_ID")
+    df.to_csv("Dataset-Final/merge_customer_summary.csv", index=False)
+
+Merge_Customer_summary()
